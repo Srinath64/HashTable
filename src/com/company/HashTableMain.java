a6e0b7b200446d5a0d7564183d9e2e57a278b6ee
Srinath S <srinath@Srinaths-MacBook-Air.local>
2021-12-22 18:21:53 +0530

    First Commit

---
+++ src/com/company/HashTableMain.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+
+        Hashtable<String , Integer> hash = new Hashtable();
+
+        String message = "To be or not to be";
+
+
+        String[] messageArray = message.toLowerCase().split(" ");
+
+        for(String word: messageArray) {
+
+            Integer value =  hash.get(word);
+
+            if( value == null)
+                value = 1;
+            else
+                value = value + 1;
+            hash.add(word , value);
+        }
+        System.out.println(hash);
+    }
+}
